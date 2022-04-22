e2b17cc1d7a3dd7e5522bad691fd2345a4b2cd62
Bernardo Abreu <bernardo.abreu@trustly.com>
2022-04-22 15:51:37 -0300

    Initial settings

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+type Repository = {
+  full_name: string;
+  description: string;
+}
+
+function App() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/bertrindade/repos")
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, []);
+
+  return (
+    <ul>
+      {repositories.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default App
